1cea6de40be41d7ade63f5beb6e7c2cd0ad64e67
stevecool2020 <stephen.cool@colorado.edu>
2020-11-15 23:41:49 -0500

    Fixing bug with recipe and buildroot config

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 2f360245a7249b91fdb845d38b876a4a054bd030
+AESD_ASSIGNMENTS_VERSION = 32df4c65bdd6da7c0a5a751e4286eed256a02e98
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
