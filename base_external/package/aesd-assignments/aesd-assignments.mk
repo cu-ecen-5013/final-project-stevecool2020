60084c7fc7b3ab0a18ef9d104baf4f71eea0ced2
adityapande-1995 <aditya050995@gmail.com>
2020-11-28 23:51:00 -0700

    transmission between 2 pi(s) tested

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 594078a1c2470d29af44263f32580e97506b620c
+AESD_ASSIGNMENTS_VERSION = ad6e85121e3688b36c5abc817c5cca2e31ae8ba6
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,7 +20,7 @@ endef
 
 # TODO add your writer, finder and tester utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/file-transfer-app $(@D)/morse-loopback $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/file-transfer-app $(@D)/morse-loopback $(@D)/cleanup $(@D)/receiver $(TARGET_DIR)/usr/bin/
 endef
 
 
